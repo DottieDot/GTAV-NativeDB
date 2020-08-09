8349e76b10be718277105a43a6fb1c975733dd25
Taran van Groenigen <tvangroenigen@outlook.com>
2020-08-09 22:01:14 +0200

    Add github link

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography, makeStyles, Link } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, makeStyles, Link, IconButton } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 import { Link as RouterLink } from 'react-router-dom'
+import { GitHub as GithubIcon } from '@material-ui/icons'
 
 const useStyles = makeStyles(theme => ({
   appBar: {
@@ -10,6 +11,9 @@ const useStyles = makeStyles(theme => ({
   subAppbar: {
     background: theme.palette.background.default
   },
+  githubButton: {
+    marginLeft: theme.spacing(1)
+  }
 }))
 
 export default ({ toolbarRef }) => {
@@ -34,6 +38,15 @@ export default ({ toolbarRef }) => {
               GTA V Native Reference
             </Link>
           </Typography>
+          <IconButton 
+            aria-label="View on Github" 
+            color="inherit"
+            className={classes.githubButton}
+            href="https://github.com/DottieDot/GTAV-NativeDB"
+            target="_blank"
+          >
+            <GithubIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
       <AppBar
