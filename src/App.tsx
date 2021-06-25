8e5d173d4831a513dba2410bccf53aa57c4a60b2
Taran van Groenigen <tvangroenigen@outlook.com>
2021-06-25 15:04:01 +0200

    Scroll to selected native

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import { CssBaseline } from '@material-ui/core'
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Provider as StoreProvider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { AppBar } from './components'
+import useNamespaces from './hooks/useNamespaces'
 import NativeLoader from './NativeLoader'
 import Router from './Router'
 import store from './store'
 import Theme from './Theme'
 
+function LoadGate() {
+  const namespaces = useNamespaces()
+  if (!Object.keys(namespaces).length) {
+    return (
+      <h1>Loading {Object.keys(namespaces).length}</h1>
+    )
+  }
+
+  return (
+    <Fragment>
+      <AppBar />
+      <Router />
+    </Fragment>
+  )
+}
 
 export default function App() {
   return (
@@ -16,8 +32,7 @@ export default function App() {
         <Theme>
           <CssBaseline />
           <NativeLoader />
-          <AppBar />
-          <Router />
+          <LoadGate />
         </Theme>
       </BrowserRouter>
     </StoreProvider>
--- src/components/NativeList/NativeList.tsx
+++ src/components/NativeList/NativeList.tsx
@@ -1,7 +1,10 @@
 import { Box, BoxProps } from '@material-ui/core'
-import React, { memo, useCallback, useMemo } from 'react'
+import React, { memo, useCallback, useMemo, useState, useEffect } from 'react'
+import { useRef } from 'react'
+import { useParams } from 'react-router-dom'
 import AutoSizer from 'react-virtualized-auto-sizer'
 import { StickyTree } from 'react-virtualized-sticky-tree'
+import { useNative } from '../../hooks'
 import { Namespace } from '../../store'
 import NativeHeader from '../NativeHeader'
 import NativeListItem from './NativeListItem'
@@ -23,6 +26,42 @@ function NativeList({ namespaces, sx = {}, ...rest }: NativeListProps) {
       }))
       return accumulator
     }, {}), [namespaceArray])
+  const { native: selectedNativeHash } = useParams<{ native: string } >()
+  const selectedNative = useNative(selectedNativeHash)
+  const [scrollTop, setScrollTop] = useState(-1)
+  const [hasScrolledToNative, setHasScrolledToNative] = useState(false)
+  const [listLoaded, setListLoaded] = useState(false)
+  const listRef = useRef<StickyTree>(null)
+
+  useEffect(() => {
+    if (!listLoaded || hasScrolledToNative) {
+      return
+    }
+    setHasScrolledToNative(true)
+    if (!selectedNative) {
+      return
+    }
+
+    let index = 0
+    let found = false
+    for (const ns of namespaceArray) {
+      if (ns.name === selectedNative.namespace) {
+        index += ns.natives.indexOf(selectedNative.hash) + 2
+        found = true
+        break
+      }
+      index += ns.natives.length + 1
+    }
+    if (found) {
+      setScrollTop(index)
+    }
+  }, [setScrollTop, namespaceArray, selectedNative, hasScrolledToNative, setHasScrolledToNative, listLoaded])
+
+  useEffect(() => {
+    if (scrollTop !== -1) {
+      setScrollTop(-1)
+    }
+  }, [scrollTop, setScrollTop])
 
   const getChildren = useCallback(({ id }) => {
     if (id === 'root') {
@@ -40,6 +79,10 @@ function NativeList({ namespaces, sx = {}, ...rest }: NativeListProps) {
   }, [namespaceData, namespaceArray])
 
   const renderRow = useCallback(({ node: { id }, style }) => {
+    if (!listLoaded) {
+      setListLoaded(true)
+    }
+
     if (id && (id[0] !== '0')) {
       return (
         <NativeHeader 
@@ -58,7 +101,7 @@ function NativeList({ namespaces, sx = {}, ...rest }: NativeListProps) {
         key={id}
       />
     )
-  }, [namespaces])
+  }, [namespaces, listLoaded, setListLoaded])
 
   return (
     <Box sx={{ flex: 1, ...sx }} {...rest}>
@@ -66,6 +109,8 @@ function NativeList({ namespaces, sx = {}, ...rest }: NativeListProps) {
         {({ height, width }) => (
           <StickyTree
             root={{ node: { id: 'root' }, height: 0 }}
+            scrollIndex={scrollTop}
+            ref={listRef}
             width={width}
             height={height}
             getChildren={getChildren}
