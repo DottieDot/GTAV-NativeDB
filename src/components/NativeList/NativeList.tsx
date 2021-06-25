9f2e7d55eb4e3c65c75fd0555276fdbaefbabe64
Taran van Groenigen <tvangroenigen@outlook.com>
2021-06-25 15:12:09 +0200

    Improve scroll to native

--- src/components/NativeList/NativeList.tsx
+++ src/components/NativeList/NativeList.tsx
@@ -4,7 +4,6 @@ import { useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import AutoSizer from 'react-virtualized-auto-sizer'
 import { StickyTree } from 'react-virtualized-sticky-tree'
-import { useNative } from '../../hooks'
 import { Namespace } from '../../store'
 import NativeHeader from '../NativeHeader'
 import NativeListItem from './NativeListItem'
@@ -27,43 +26,24 @@ function NativeList({ namespaces, sx = {}, ...rest }: NativeListProps) {
       return accumulator
     }, {}), [namespaceArray])
   const { native: selectedNativeHash } = useParams<{ native: string } >()
-  const selectedNative = useNative(selectedNativeHash)
-  const [scrollTop, setScrollTop] = useState(-1)
   const [hasScrolledToNative, setHasScrolledToNative] = useState(false)
   const [listLoaded, setListLoaded] = useState(false)
   const listRef = useRef<StickyTree>(null)
 
   useEffect(() => {
-    if (!listLoaded || hasScrolledToNative) {
+    if (hasScrolledToNative || !listRef.current || !listLoaded) {
       return
     }
     setHasScrolledToNative(true)
-    if (!selectedNative) {
-      return
-    }
 
-    let index = 0
-    let found = false
-    for (const ns of namespaceArray) {
-      if (ns.name === selectedNative.namespace) {
-        index += ns.natives.indexOf(selectedNative.hash) + 2
-        found = true
-        break
-      }
-      index += ns.natives.length + 1
-    }
-    if (found) {
-      setScrollTop(index)
-    }
-  }, [setScrollTop, namespaceArray, selectedNative, hasScrolledToNative, setHasScrolledToNative, listLoaded])
-
-  useEffect(() => {
-    if (scrollTop !== -1) {
-      setScrollTop(-1)
-    }
-  }, [scrollTop, setScrollTop])
+    listRef.current.scrollNodeIntoView(selectedNativeHash)
+  }, [listRef, selectedNativeHash, hasScrolledToNative, listLoaded, setHasScrolledToNative])
 
   const getChildren = useCallback(({ id }) => {
+    if (!listLoaded) {
+      setListLoaded(true)
+    }
+
     if (id === 'root') {
       return namespaceArray.map(ns => ({
         node: {
@@ -76,13 +56,9 @@ function NativeList({ namespaces, sx = {}, ...rest }: NativeListProps) {
     else if (id && (id[0] !== '0')) {
       return namespaceData[id]
     }
-  }, [namespaceData, namespaceArray])
+  }, [namespaceData, namespaceArray, listLoaded, setListLoaded])
 
   const renderRow = useCallback(({ node: { id }, style }) => {
-    if (!listLoaded) {
-      setListLoaded(true)
-    }
-
     if (id && (id[0] !== '0')) {
       return (
         <NativeHeader 
@@ -101,7 +77,7 @@ function NativeList({ namespaces, sx = {}, ...rest }: NativeListProps) {
         key={id}
       />
     )
-  }, [namespaces, listLoaded, setListLoaded])
+  }, [namespaces])
 
   return (
     <Box sx={{ flex: 1, ...sx }} {...rest}>
@@ -109,7 +85,6 @@ function NativeList({ namespaces, sx = {}, ...rest }: NativeListProps) {
         {({ height, width }) => (
           <StickyTree
             root={{ node: { id: 'root' }, height: 0 }}
-            scrollIndex={scrollTop}
             ref={listRef}
             width={width}
             height={height}
